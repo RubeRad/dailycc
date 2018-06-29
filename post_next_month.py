a87e4ebe4b60c6aa0902635ee1a015d22bd92d94
Reuben Settergren <ruberad@gmail.com>
2018-06-28 21:21:21 -0700

    Test creating new posts with html content

--- post_next_month.py
+++ post_next_month.py
@@ -4,16 +4,28 @@
 
 from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import NewPost
+from datetime import datetime
+
+def slurp(fname):
+    file = open(fname)
+    contents = file.read()
+    return contents
+
 
 dcurl = "https://dailyconfession.wordpress.com/xmlrpc.php"
 dwurl = "https://dailywestminster.wordpress.com/xmlrpc.php"
 testurl = "https://dcimporttest.wordpress.com/xmlrpc.php"
-pw    = input("Enter password: ")
+try:    pw = slurp('password.txt')
+except: pw = input("Enter password: ")
 wp = Client(testurl, 'RubeRad', pw)
 
 post = WordPressPost()
-post.title = 'test'
-post.content = 'testing 1 2 3'
+post.title = 'test6'
+post.content = slurp('sc/w1.html')
+dt = datetime(2018,7,1,9,0,0,0) # 9am GMT is 4/1am Eastern/Pacific
+post.date = post.date_modified = dt
+post.post_status = 'publish'
+post.comment_status = 'closed'
 wp.call(NewPost(post))
 
 
