066b2373186e869c7655527f14bfd94074114f4d
Reuben Settergren <ruberad@gmail.com>
2018-06-28 22:01:54 -0700

    hardcode uploads for jul 2018 buy time to make script more general

--- post_next_month.py
+++ post_next_month.py
@@ -5,31 +5,51 @@
 from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import NewPost
 from datetime import datetime
+import glob
+import re
 
 def slurp(fname):
     file = open(fname)
     contents = file.read()
     return contents
 
+def post_file(wp, f, t, y, m, d):
+    p = WordPressPost()
+    p.title = t
+    p.content = slurp(f)
+    # 9am zulu is early Eastern-Pacific
+    p.date = p.date_modified = datetime(y,m,d,9)
+    p.post_status = 'publish'
+    p.comment_status = 'closed'
+    wp.call(NewPost(p))
 
 dcurl = "https://dailyconfession.wordpress.com/xmlrpc.php"
 dwurl = "https://dailywestminster.wordpress.com/xmlrpc.php"
 testurl = "https://dcimporttest.wordpress.com/xmlrpc.php"
 try:    pw = slurp('password.txt')
 except: pw = input("Enter password: ")
-wp = Client(testurl, 'RubeRad', pw)
 
-post = WordPressPost()
-post.title = 'test6'
-post.content = slurp('sc/w1.html')
-dt = datetime(2018,7,1,9,0,0,0) # 9am GMT is 4/1am Eastern/Pacific
-post.date = post.date_modified = dt
-post.post_status = 'publish'
-post.comment_status = 'closed'
-wp.call(NewPost(post))
+dw = Client(dwurl, 'RubeRad', pw)
+dwjul = glob.glob("w/jul*_esv.html")
+dwjul.sort()
+for i in range(len(dwjul)):
+    d = i+1
+    title = "Daily Westminster 7/" + str(d) + "/2018"
+    print(title, " ", dwjul[i])
+    post_file(dw, dwjul[i], title, 2018, 7, d)
 
-
-
-
-
-
+dc = Client(dcurl, 'RubeRad', pw)
+dcfile = open('201807.txt')
+for i in range(31):
+    d = i+1
+    fname = dcfile.readline().rstrip()
+    if   re.match("cc", fname): title = "Children's Catechism"
+    elif re.match("sc", fname): title = "Shorter Catechism"
+    elif re.match("lc", fname): title = "Larger Catechism"
+    elif re.match("wc", fname): title = "Westminster Confession"
+    elif re.match("so", fname): title = "Synod of Dordt"
+    elif re.match("bc", fname): title = "Belgic Confession"
+    elif re.match("hc", fname): title = "Heidelberg Confession"
+    title += " 7/" + str(d) + "/2018"
+    print(title, " ", fname)
+    post_file(dc, fname, title, 2018, 7, d)
