f6cac9c2211824823f81bbb7dfecc7ec326e8888
Reuben Settergren <ruberad@gmail.com>
2018-07-28 10:22:16 -0700

    Move DW within the same loop as DC

--- post_next_month.py
+++ post_next_month.py
@@ -23,12 +23,18 @@ def post_file(wp, f, t, y, m, d):
     p.comment_status = 'closed'
     wp.call(NewPost(p))
 
+freal = False
+# login to wordpress
 dcurl = "https://dailyconfession.wordpress.com/xmlrpc.php"
 dwurl = "https://dailywestminster.wordpress.com/xmlrpc.php"
 testurl = "https://dcimporttest.wordpress.com/xmlrpc.php"
 try:    pw = slurp('password.txt')
 except: pw = input("Enter password: ")
+if freal:
+    dw = Client(dwurl, 'RubeRad', pw)
+    dc = Client(dcurl, 'RubeRad', pw)
 
+# prepare to loop through days of year
 tt = date.today().timetuple()
 year   = tt[0]
 thismo = tt[1]  # 1..12
@@ -40,30 +46,20 @@ monthnames = {1:'jan',2:'feb',3:'mar',4:'apr',5:'may',6:'jun',
               7:'jul',8:'aug',9:'sep',10:'oct',11:'nov',12:'dec'}
 monam = monthnames[nextmo]
 
-dw = Client(dwurl, 'RubeRad', pw)
-dwfiles = glob.glob("w/" + monam + "*_esv.html")
-dwfiles.sort()
-for i in range(len(dwfiles)):
-    d = i+1
-    title = "Daily Westminster, " + monam + " " + str(d) + " " + str(year)
-    print(title, " ", dwfiles[i])
-    #post_file(dw, dwfiles[i], title, 2018, 7, d)
+d      = date(year,1,1)      # count up from Jan 1
+oneday = date(year,1,2) - d  # date delta of 1 day for incrementing
+while True:
+    if d.month > nextmo or d.year > year: # if we've made it past our month
+        break                             # stop!
+    elif d.month == nextmo: # if d is in the rith month, post a DC and a DW
+        tt = d.timetuple()
+        wday = tt[6]               # 0 = Monday
+        yday = tt[7]               # 1 = jan1
+        wnum = int((yday - 1) / 7) # 0..51
 
-
-dc = Client(dcurl, 'RubeRad', pw)
-# count up from Jan 1
-d      = date(year,1,1)
-oneday = date(year,1,2) - d
-
-for i in range(365):
-    tt = d.timetuple()
-    wday = tt[6]  # 0 = Monday
-    yday = tt[7]  # 1 = jan1
-    wnum = int((yday-1)/7)
-
-    if d.month == nextmo:
-        w = str(wnum+1)
-        bnum = wnum
+        # determine the appropriate Daily Confession for this day of current week #
+        w = str(wnum+1) # most go weeks 1-52
+        bnum = wnum # Belgic goes weeks 1-26 twice a year
         if bnum >= 26: bnum -= 26
         bw= str(bnum+1)
 
@@ -74,10 +70,19 @@ for i in range(365):
         elif wday == 4: fname = 'sod/w' + w + '.html'; title=     "Canons of Dordt, Week "+w
         elif wday == 5: fname = 'wcf/w' + w + '.html'; title="Westminster Confession, Week"+w
         elif wday == 6: fname = 'hc/ld' + w + '.html'; title="Heidelberg Catechism, Lord's Day "+w
-        print(title + ' ' + fname)
-        # post_file(dw, fname, title, d.year, d.month, d.day)
+        print(fname + '\t' + title)
+        if freal:
+            post_file(dc, fname, title, d.year, d.month, d.day)
 
-    # even if not in the current month, keep counting
-    d += oneday
+        # determine appropriate Daily Westminster for this day of the year
+        month = monthnames[d.month] # number-->name
+        fname = "w/" + month
+        if d.day < 10: fname += '0'
+        fname += str(d.day) + '_esv.html'
+        Month = month[0].upper() + month[1] + month[2]
+        title = 'Daily Westminster, ' + Month + ' ' + str(d.day)
+        print(fname + '\t' + title)
+        if freal:
+            post_file(dw, fname, title, d.year, d.month, d.day)
 
-
+    d += oneday # keep counting
